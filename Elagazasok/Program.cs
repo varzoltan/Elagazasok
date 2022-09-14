4fb95ca352b8796338cd8f82bd36777481c117e3
varzoltan <64203391+varzoltan@users.noreply.github.com>
2022-09-14 14:23:31 +0200

    2022.09.14., 14:23

--- Elagazasok/Program.cs
+++ Elagazasok/Program.cs
@@ -71,6 +71,77 @@ namespace Elagazasok
             }
 
             //switch - case utasítás házi feladat
+            Console.Write("Kérem adja meg a hét napját (1 - 7): ");
+            string hetNapja = Console.ReadLine();
+            int hetNapja_int = Convert.ToInt16(hetNapja);
+            char[] hetNapja_char = hetNapja.ToCharArray();
+            switch (hetNapja)//String típussal,szöveges változóval
+            {
+                case "1": Console.WriteLine("Hétfő"); break;
+                case "2": Console.WriteLine("Kedd"); break;
+                case "3": Console.WriteLine("Szerda"); break;
+                case "4": Console.WriteLine("Csütörtök"); break;
+                case "5": Console.WriteLine("Péntek"); break;
+                case "6": Console.WriteLine("Szombat"); break;
+                case "7": Console.WriteLine("Vasárnap"); break;
+                //Elhagyható, nem kötelező a default ág!
+                default: Console.WriteLine("Nem 1 - 7 közötti számot adott meg");break;
+            }
+
+            switch (hetNapja_int)//Egész számmal kezelve, int típus
+            {
+                case 1: Console.WriteLine("Hétfő"); break;
+                case 2: Console.WriteLine("Kedd"); break;
+                case 3: Console.WriteLine("Szerda"); break;
+                case 4: Console.WriteLine("Csütörtök"); break;
+                case 5: Console.WriteLine("Péntek"); break;
+                case 6: Console.WriteLine("Szombat"); break;
+                case 7: Console.WriteLine("Vasárnap"); break;
+                //Elhagyható, nem kötelező a default ág!
+                default: Console.WriteLine("Nem 1 - 7 közötti számot adott meg"); break;
+            }
+
+            switch (hetNapja_char[0])//Egész számmal kezelve, int típus
+            {
+                case '1': Console.WriteLine("Hétfő"); break;
+                case '2': Console.WriteLine("Kedd"); break;
+                case '3': Console.WriteLine("Szerda"); break;
+                case '4': Console.WriteLine("Csütörtök"); break;
+                case '5': Console.WriteLine("Péntek"); break;
+                case '6': Console.WriteLine("Szombat"); break;
+                case '7': Console.WriteLine("Vasárnap"); break;
+                //Elhagyható, nem kötelező a default ág!
+                default: Console.WriteLine("Nem 1 - 7 közötti számot adott meg"); break;
+            }
+
+            //3.típusu elágazás, melyet kiírásnál szoktunk alkalmazni.
+            Console.WriteLine((x<y) ? "igaz" : "hamis");
+
+            /*Kérjen be a felhasználótól 3 egész számot. 
+             * Írja a képernyőre a legnagyobb, majd a következő legynagyobb, majd végül
+             * a közülül legkisebb számot, szóközzel elválasztva egymástól.
+             */
+            Console.Write("Kérem adja meg az első számot: ");
+            int szam_1 = int.Parse(Console.ReadLine());
+            Console.Write("Kérem adja meg az első számot: ");
+            int szam_2 = int.Parse(Console.ReadLine());
+            Console.Write("Kérem adja meg az első számot: ");
+            int szam_3 = int.Parse(Console.ReadLine());
+
+            if (szam_1 > szam_2 && szam_1 > szam_3)
+            {
+                Console.Write(szam_1 + " ");
+                if (szam_2 > szam_3)
+                {
+                    Console.Write(szam_2 + " " + szam_3);
+                }
+                else
+                {
+                    Console.Write(szam_3 + " " + szam_2);
+                }
+            }
+                
+
 
             Console.ReadKey();
         }
